32212cdf0a8b1efb7c0c9f1428415f6e6d660e8d
paxxchal <paschalgabrielacj8@gmail.com>
2024-03-07 16:04:38 +0100

    pushed header file

--- sort.h
+++ sort.h
@@ -31,5 +31,8 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
+void swap(int *a, int *b);
+int partition(int *arr, int low, int high, size_t size);
+void quickSort(int *arr, int low, int high, size_t size);
 
 #endif /* SORT_H */
